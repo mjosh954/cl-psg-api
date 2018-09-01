28d1dcf5d2511e9ee8b422bf427aab0fb66ea67a
Josh <mjosh954@gmail.com>
2018-09-01 10:14:58 -0500

    init

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject cl-back "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :main ^:skip-aot cl-back.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
