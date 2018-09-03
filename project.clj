9b57b55076c8ff0262f54481bfc52682debca98f
Josh <mjosh954@gmail.com>
2018-09-02 22:04:25 -0500

    added db fetch command to http handler

--- project.clj
+++ project.clj
@@ -7,7 +7,10 @@
     [ring/ring-core "1.6.3"]
     [ring/ring-jetty-adapter "1.6.3"]
     [ring/ring-json "0.4.0"]
-    [com.spoon16/clj-gson "0.0.1"]]
+    [korma "0.4.3"]
+    [org.postgresql/postgresql "9.2-1002-jdbc4"]
+    [com.spoon16/clj-gson "0.0.1"]
+    [environ "1.1.0"]]
   :main ^:skip-aot cl-back.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/cl_back/core.clj
+++ src/cl_back/core.clj
@@ -1,16 +1,31 @@
 (ns cl-back.core
   (:use ring.middleware.params
-    ring.adapter.jetty)) 
+    ring.adapter.jetty
+    korma.db
+    korma.core)) 
 
 (use '[clj-gson.json :only (to-json from-json)])
 (require '[ring.middleware.json :refer [wrap-json-response]]
     '[ring.util.response :refer [response]])
 
-    
+(defdb db (postgres {
+  :host "localhost"
+  :db "clbackdb"
+  :user "postgres"
+  :password "foobar"
+  :port "5432"
+  :delimiters ""
+}))
+
+(defentity clojurecommand
+  (entity-fields :command_id :name :command :description :example))
 
 
-(defn handler [request]
-  (response {:foo request}))
+(defn find-command [command]
+  (select clojurecommand
+    (where {:command command})))
 
-               
-(run-jetty (wrap-json-response handler) {:port 3000})
+(defn handler [{{q "q"} :params}]
+  (-> (response (find-command q))))
+
+(run-jetty (wrap-json-response (wrap-params handler)) {:port 3000})
